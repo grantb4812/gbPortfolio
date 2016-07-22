4d37ed41a970bb4372a0c941bb11b47eaab6accf
grantb4812 <grantb4812@gmail.com>
2016-07-22 08:13:06 -0500

    working on sliding pages

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 $(document).ready(function() {
+  
+
   $('.collapsed-menu-bar').on('click', function() {
     $('.collapsed-menu').toggle('slow')
+  });
+
+  $('.header-content .down-arrow').click(function() {
+    $('.header-content').slideUp(500);
+    $('.portfolio').slideDown(2500);
+  });
+
+  $('.portfolio .up-arrow').click(function() {
+    $('.portfolio').slideUp(500);
+    $('.header-content').slideDown(2500);
+  });
+
+  $('.portfolio .down-arrow').click(function() {
+    $('.portfolio').slideUp('slow');
+    $('.about').slideDown('1000');
   });
   
 });
