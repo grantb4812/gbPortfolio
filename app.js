c20a1d0c0c9d157e844d878055407a42cf90fe8f
grantb4812 <grantb4812@gmail.com>
2016-07-22 13:01:54 -0500

    working on mobile

--- app.js
+++ app.js
@@ -1,23 +1,54 @@
 $(document).ready(function() {
-  
+/*
+  $(window).scroll(function() {
+    $('.portfolio').css('display', 'block');
+    $('.about').css('display', 'block');
+    $('.contact').css('display', 'block');
 
+  });
+*/
   $('.collapsed-menu-bar').on('click', function() {
     $('.collapsed-menu').toggle('slow')
   });
 
   $('.header-content .down-arrow').click(function() {
-    $('.header-content').slideUp(500);
-    $('.portfolio').slideDown(2500);
+    $('.header-content').slideUp('slow', function() {
+      $('.portfolio').slideDown('slow');
+    });
+    
   });
 
   $('.portfolio .up-arrow').click(function() {
-    $('.portfolio').slideUp(500);
-    $('.header-content').slideDown(2500);
+    $('.portfolio').slideUp('slow', function() {
+      $('.header-content').slideDown('slow');
+    });
+    
   });
 
   $('.portfolio .down-arrow').click(function() {
-    $('.portfolio').slideUp('slow');
-    $('.about').slideDown('1000');
+    $('.portfolio').slideUp('slow', function() {
+      $('.about').slideDown('1000');
+    });
   });
+
+  $('.about .up-arrow').click(function() {
+    $('.about').slideUp('slow', function() {
+      $('.portfolio').slideDown('slow');
+    });
+  });
+
+  $('.about .down-arrow').click(function() {
+    $('.about').slideUp('slow', function() {
+      $('.contact').slideDown('slow');
+    });
+  });
+
+  $('.contact .up-arrow').click(function() {
+    $('.contact').slideUp('slow', function() {
+      $('.about').slideDown('slow');
+    });
+  });
+
+  
   
 });
